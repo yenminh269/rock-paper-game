cdb78e2b0a17857719c3e46a80ad3aff3115b2e9
Minh Ho <167224877+yenminh269@users.noreply.github.com>
2025-01-09 19:29:36 -0600

    Add 3 files

--- java.js
+++ java.js
@@ -1,55 +1,46 @@
-function getHumanChoice(){
-    let user = prompt("What's your choice: ");
-    if(user == "rock"|| user =="Rock"){
-        return 1;
-    }
-    else if(user == "paper"|| user == "Paper"){
-        return 2;
-    }
-    else if(user == "scissors"||user =="scissors"){
-        return 3;
-    }
-    else{
-        console.log("Please type the choice: ");
-    }
-
-   
-}
 function getComputerChoice(){
     return Math.floor(Math.random()*3)+1;
-}
+} 
 
-function playGame(humanChoice, computerChoice){
-    let i = 0;
-    let humanSelection;
-    let computerSelection;
-    while(i<5){
-        humanSelection = humanChoice();
-        computerSelection = computerChoice();
+function playGame(humanSelection, computerChoice){
+    
+    let computerSelection = computerChoice();
+    do{
         if((humanSelection == 1 && computerSelection == 3)||(humanSelection == 2 && computerSelection ==1) ||(humanSelection == 3 && computerSelection ==2)){
-            console.log("You wins");
+            message.textContent = "You win";
             humanScore +=1;
+            player_Score.textContent = humanScore;
+
         }
         else if((humanSelection == 1 && computerSelection == 2)||(humanSelection == 2 && computerSelection ==3)||(humanSelection == 3 && computerSelection ==1)){
-            console.log("Computer wins");
+            message.textContent = "You lose";
             computerScore +=1;
+            computer_Score = computerScore;
+
         }
         else{
-            console.log("Tied");
+            message.textContent = "It's a tied";
         }
-        i +=1;
-    }
+    }while(humanScore != 5 || computerScore != 5);
     if(humanScore > computerScore){
-        console.log(`Overall: You wins. Total score: ${humanScore}`);
+        message.textContent = `You are the winner. Total score: ${humanScore}`;
     }
     else if(computerScore > humanScore){
-        console.log(`Overall: Computer wins. Total score: ${computerScore}`);
+        message.textContent = `The computer is the winner. Total score: ${computerScore}`;
     }
     else {
-        console.log(`Overall: The game is tied. Your Score: ${humanScore}. Computer Score: ${computerScore}`);
+        message.textContent =`Overall: The game is tied. Your Score: ${humanScore}. Computer Score: ${computerScore}`;
     }
    
 }
 let humanScore = 0;
 let computerScore = 0;
-playGame(getHumanChoice,getComputerChoice); /* pass call-back function*/
+let player_Score = document.querySelector("#player");
+let computer_Score = document.querySelector("#computer");
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+let message = document.querySelector("#message");
+rock.addEventListener("click", playGame(1,getComputerChoice));
+paper.addEventListener("click", playGame(2,getComputerChoice));
+scissors.addEventListener("click", playGame(3,getComputerChoice));

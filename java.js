9100a676dabb7b609e0a80cd8f4a101139779cfc
Minh Ho <167224877+yenminh269@users.noreply.github.com>
2025-01-09 22:25:49 -0600

    fix the add event function

--- java.js
+++ java.js
@@ -2,37 +2,39 @@ function getComputerChoice(){
     return Math.floor(Math.random()*3)+1;
 } 
 
-function playGame(humanSelection, computerChoice){
-    
-    let computerSelection = computerChoice();
-    do{
+function playGame(humanSelection){
+    let computerSelection = getComputerChoice();
+
+    myLoop: 
+    if(humanScore != 5 || computerScore != 5){
         if((humanSelection == 1 && computerSelection == 3)||(humanSelection == 2 && computerSelection ==1) ||(humanSelection == 3 && computerSelection ==2)){
             message.textContent = "You win";
             humanScore +=1;
-            player_Score.textContent = humanScore;
+            player_Score.textContent = "Player Score: " + humanScore + "points";
 
         }
         else if((humanSelection == 1 && computerSelection == 2)||(humanSelection == 2 && computerSelection ==3)||(humanSelection == 3 && computerSelection ==1)){
             message.textContent = "You lose";
             computerScore +=1;
-            computer_Score = computerScore;
+            computer_Score.textContent = "Computer Score: " + computerScore + "points";
 
         }
         else{
             message.textContent = "It's a tied";
         }
-    }while(humanScore != 5 || computerScore != 5);
-    if(humanScore > computerScore){
-        message.textContent = `You are the winner. Total score: ${humanScore}`;
+        break myLoop;
     }
-    else if(computerScore > humanScore){
-        message.textContent = `The computer is the winner. Total score: ${computerScore}`;
-    }
+    if(humanScore == 5){
+        message.textContent = `You are the winner. Total score: ${humanScore} points`;
+        }
+    else if(computerScore == 5){
+        message.textContent = `The computer is the winner. Total score: ${computerScore} points`;
+        }
     else {
-        message.textContent =`Overall: The game is tied. Your Score: ${humanScore}. Computer Score: ${computerScore}`;
+        return;
     }
-   
 }
+
 let humanScore = 0;
 let computerScore = 0;
 let player_Score = document.querySelector("#player");
@@ -41,6 +43,8 @@ let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
 let message = document.querySelector("#message");
-rock.addEventListener("click", playGame(1,getComputerChoice));
-paper.addEventListener("click", playGame(2,getComputerChoice));
-scissors.addEventListener("click", playGame(3,getComputerChoice));
+
+rock.addEventListener("click",() => playGame(1));
+scissors.addEventListener("click",() => playGame(3));
+paper.addEventListener("click",() => playGame(2));
+
